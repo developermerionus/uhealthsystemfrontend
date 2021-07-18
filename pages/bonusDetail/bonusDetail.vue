a0463d2da12370b502664f96d79a03fbb97b902e
fulei888 <fulei888@churchofjesuschrist.org>
2021-07-17 23:44:18 -0700

    upgrade bonusDetail show 4 weeks
    upgrade bonusDetail show 4 weeks

--- pages/bonusDetail/bonusDetail.vue
+++ pages/bonusDetail/bonusDetail.vue
@@ -33,7 +33,7 @@
 		                    <t-th>{{$lang('common.week_of_bonus')}}</t-th>
 		                    <t-th>{{$lang('common.start_bonus')}}</t-th>
 		                    <t-th>{{$lang('common.end_bonus')}}</t-th>
-		                    <t-th>{{$lang('common.retail_bonus')}}</t-th>
+		                  <!--  <t-th>{{$lang('common.retail_bonus')}}</t-th> -->
 							<t-th>{{$lang('common.fast_bonus')}}</t-th>
 							<t-th>{{$lang('common.leadership_bonus')}}</t-th>
 							<t-th>{{$lang('common.team_bonus')}}</t-th>
@@ -47,11 +47,11 @@
 		                    <t-td>{{ item.week}}</t-td>
 		                    <t-td>{{ item.start}}</t-td>
 		                    <t-td>{{ item.end}}</t-td>
+							<!-- <t-td>{{ item.retail_bonus }}</t-td> -->
 		                    <t-td>{{ item.fast_bonus }}</t-td>
 							<t-td>{{ item.leadership_bonus }}</t-td>
 							<t-td>{{ item.team_bonus }}</t-td>
 							<t-td>{{ item.ranking_bonus }}</t-td>
-							<t-td>{{ item.retail_bonus }}</t-td>
 							<t-td>{{ item.cash_bonus }}</t-td>
 		                </t-tr>
 		            </t-table>
@@ -87,49 +87,65 @@
 					totalBonus: 0,
 					showListOption: [],
 					optionRangeArr: [],
-					pastFourWeeks : [],
-					pastEightWeeks: [],
-					pastTwelvelWeeks: [],
-					pastSixteenWeeks: [],
-					allWeeksList : [],
-					past_N_weeks:0,//0 stand for get all bonus detail
-					selectTypeLists:[
-										{value:"0",name:'银行卡提现（默认方式）'},
-										{value:"1",name:'微信提现'}
-									],
+					// pastFourWeeks : [],
+					// pastEightWeeks: [],
+					// pastTwelvelWeeks: [],
+					// pastSixteenWeeks: [],
+					//allWeeksList : [],
+					pastOneWeeks : [],
+					pastTwoWeeks: [],
+					pastThreeWeeks: [],
+					pastFourWeeks: [],
+					past_N_weeks:4,//0 stand for get all bonus detail
 				};
 			},
 		    onShow(){
 				_self = this;
 				this.getBonusDetail();
-				this.getBonusSum(0);
+				//this.getBonusSum(0);
+				this.getBonusSum(4);
 			},
 			created() {
 						this.editSelectTypeLists = _self.showDefualtTextNum; /* 默认选中 */
 			        },
 			methods: {
 				selectTypeChange(){
-								// console.log('this.$refs.addRequestState.value',this.$refs.addRequestState.value)
-								 if(this.$refs.addRequestState.value==4){
-									 this.showListOption = _self.pastFourWeeks;
+								// if(this.$refs.addRequestState.value==4){
+								// 	 this.showListOption = _self.pastFourWeeks;
+								// 	 this.getBonusSum(4);
+								//  }
+								//  else if (this.$refs.addRequestState.value==8){
+								// 	 this.showListOption = _self.pastEightWeeks;
+								// 	 this.getBonusSum(8);
+								//  }
+								//  else if (this.$refs.addRequestState.value==12){
+								//  	 this.showListOption = _self.pastTwelvelWeeks;
+								// 	 this.getBonusSum(12);
+								//  }
+								//  else if (this.$refs.addRequestState.value==16){
+								//  	 this.showListOption = _self.pastSixteenWeeks;
+								// 	 this.getBonusSum(16);
+								//  }
+								//  else if (this.$refs.addRequestState.value==0){
+								//  	  this.showListOption = _self.allWeeksList;
+								// 	  this.getBonusSum(0);
+								//  }
+								
+								if(this.$refs.addRequestState.value==1){
+									 this.showListOption = _self.pastOneWeeks;
+									 this.getBonusSum(1);
+								 }
+								 else if (this.$refs.addRequestState.value==2){
+									 this.showListOption = _self.pastTwoWeeks;
+									 this.getBonusSum(2);
+								 }
+								 else if (this.$refs.addRequestState.value==3){
+								 	 this.showListOption = _self.pastThreeWeeks;
+									 this.getBonusSum(3);
+								 }
+								 else if (this.$refs.addRequestState.value==4){
+								 	 this.showListOption = _self.pastFourWeeks;
 									 this.getBonusSum(4);
-									// console.log('_self.pastFourWeeks',_self.pastFourWeeks);
-								 }
-								 else if (this.$refs.addRequestState.value==8){
-									 this.showListOption = _self.pastEightWeeks;
-									 this.getBonusSum(8);
-								 }
-								 else if (this.$refs.addRequestState.value==12){
-								 	 this.showListOption = _self.pastTwelvelWeeks;
-									 this.getBonusSum(12);
-								 }
-								 else if (this.$refs.addRequestState.value==16){
-								 	 this.showListOption = _self.pastSixteenWeeks;
-									 this.getBonusSum(16);
-								 }
-								 else if (this.$refs.addRequestState.value==0){
-								 	  this.showListOption = _self.allWeeksList;
-									  this.getBonusSum(0);
 								 }
 								 },
 				change(e) {
@@ -181,38 +197,62 @@
 					
 				},
 				dividToRange(allArr) {
-					let arr = [1,2,3];
 					allArr.forEach((el, index)=> {
-						if(index<4) {
+						// if(index<4) {
+						// 	_self.pastFourWeeks.push(el);
+						// }
+						// if(index < 8) {
+						// 	_self.pastEightWeeks.push(el);
+						// }
+						// if(index < 12) {
+						// 	_self.pastTwelvelWeeks.push(el);
+						// }
+						// if(index<16) {
+						// 	_self.pastSixteenWeeks.push(el);
+						// }
+						if(index < 4) {
 							_self.pastFourWeeks.push(el);
 						}
-						if(index < 8) {
-							_self.pastEightWeeks.push(el);
+						if(index < 3) {
+							_self.pastThreeWeeks.push(el);
 						}
-						if(index < 12) {
-							_self.pastTwelvelWeeks.push(el);
+						if(index < 2) {
+							_self.pastTwoWeeks.push(el);
 						}
-						if(index<16) {
-							_self.pastSixteenWeeks.push(el);
+						if(index < 1) {
+							_self.pastOneWeeks.push(el);
 						}
 					})
-					_self.addRange (_self.pastFourWeeks, _self.pastEightWeeks, _self.pastTwelvelWeeks,_self.pastSixteenWeeks );
+					//_self.addRange (_self.pastFourWeeks, _self.pastEightWeeks, _self.pastTwelvelWeeks,_self.pastSixteenWeeks );
+					_self.addRange (_self.pastOneWeeks, _self.pastTwoWeeks, _self.pastThreeWeeks,_self.pastFourWeeks );
 				//	console.log(_self.pastSixteenWeeks);
 					},
-					addRange (fourW, eightW, twelW,sixtW ){
+					// addRange (fourW, eightW, twelW,sixtW ){
+						addRange (oneW, twoW, threeW,fourW ){
 						let rangeArr = [];
 						rangeArr.push({value: _self.showDefualtTextNum, text: this.$lang("common.pay_time_bonus"), date: ''})
-						rangeArr.push({value: 4, text:"Past 04 Weeks", date: fourW[fourW.length-1].start +" - "+ fourW[0].end })
-						if(eightW.length>4 && eightW.length<9) {
-						 rangeArr.push({value: 8, text:"Past 08 Weeks",  date: eightW[eightW.length-1].start +" - "+ eightW[0].end })
+						// rangeArr.push({value: 4, text:"Past 04 Weeks", date: fourW[fourW.length-1].start +" - "+ fourW[0].end })
+						// if(eightW.length>4 && eightW.length<9) {
+						//  rangeArr.push({value: 8, text:"Past 08 Weeks",  date: eightW[eightW.length-1].start +" - "+ eightW[0].end })
+						// }
+						// if(twelW.length>8 && twelW.length<13) {
+						//  rangeArr.push({value:12, text:"Past 12 Weeks", date:twelW[twelW.length-1].start +" - "+ twelW[0].end })
+						// }
+						// if(sixtW.length>12 && sixtW.length<17) {
+						//  rangeArr.push({value: 16, text:"Past 16 Weeks", date:sixtW[sixtW.length-1].start +" - "+ sixtW[0].end })
+						// }
+						// rangeArr.push({value: 0, text:"All", date:'' })
+						rangeArr.push({value: 1, text:"Past 01 Weeks", date: oneW[oneW.length-1].start +" - "+ oneW[0].end })
+						if(twoW.length>1 && twoW.length<3) {
+						 rangeArr.push({value: 2, text:"Past 02 Weeks",  date: twoW[twoW.length-1].start +" - "+ twoW[0].end })
 						}
-						if(twelW.length>8 && twelW.length<13) {
-						 rangeArr.push({value:12, text:"Past 12 Weeks", date:twelW[twelW.length-1].start +" - "+ twelW[0].end })
+						if(threeW.length>2 && threeW.length<4) {
+						 rangeArr.push({value:3, text:"Past 03 Weeks", date:threeW[threeW.length-1].start +" - "+ threeW[0].end })
 						}
-						if(sixtW.length>12 && sixtW.length<17) {
-						 rangeArr.push({value: 16, text:"Past 16 Weeks", date:sixtW[sixtW.length-1].start +" - "+ sixtW[0].end })
+						if(fourW.length>3 && fourW.length<5) {
+						 rangeArr.push({value: 4, text:"Past 04 Weeks", date:fourW[fourW.length-1].start +" - "+ fourW[0].end })
 						}
-						rangeArr.push({value: 0, text:"All", date:'' })
+						//rangeArr.push({value: 0, text:"All", date:'' })
 						
 						_self.optionRangeArr = rangeArr;
 					//		console.log(rangeArr);
